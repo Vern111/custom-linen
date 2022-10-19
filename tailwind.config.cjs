2997e57734847a7b5b11e2a651c6b87cc0882537
Vernon <wmvernon@proton.me>
2022-10-18 20:19:29 -0400

    Added Contact, About, and product sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,25 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
+		 screens: {
+      'sm': {'min': '480px', 'max': '767px'},
+      // => @media (min-width: 480px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
+  
 		fontFamily: {
-			'nav':['Dapifer'],
+			nav: ['Dapifer'],
 		},
 		extend: {
 			colors: {
